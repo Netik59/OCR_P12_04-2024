792fb1657781441b43fbbc38074abd4dd679f365
Netik59 <modznetik@gmail.com>
2024-05-21 16:19:16 +0200

    images updated

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -5,9 +5,9 @@ import '../../utils/style/index.css'
 export const Home = () => {
   return (
     <>
-      <section class="start">
-        <div class="overlay"></div>
-        <div class="center">
+      <section className="start">
+        <div className="overlay"></div>
+        <div className="center">
           <h1>MY WORKS</h1>
           <p>Transforming Concepts Into Memorable Creations</p>
         </div>
--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -1,15 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun, faBars } from '@fortawesome/free-solid-svg-icons'
+import logoPortfolio from '../../images/logo_portfolio100.webp'
 
 export const Header = () => {
   return (
     <header className="header">
       <a href="index.html">
-        <img
-          className="logo"
-          src="images/logo_portfolio100.png"
-          alt="Logo du header"
-        />
+        <img className="logo" src={logoPortfolio} alt="Logo du header" />
       </a>
       <div className="nav">
         <FontAwesomeIcon icon={faSun} className="icon" />
